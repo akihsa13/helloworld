a0f231da6f46a882ca1dcef8d7f144d6f5a870f3
Ashika <ashika.baraili.2023@student.lhschools.org>
2020-03-02 14:09:04 -0500

    intellij commits

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("My name is");
+        System.out.println("Ashika");
+
+        System.out.println("I helped my familly pack clothes");
+        System.out.println("we moved to Ohio from Houston");
+        System.out.println("I got a german shepherd puppy");
+
+
+
+    }
+}
